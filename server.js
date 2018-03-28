b4220d8389989625159e577c04a128eafb493219
Parichit <parichit.kumark@gmail.com>
2018-03-29 01:14:24 +0530

    Fix app variable

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500).send(err.message || 'Something broke!')
 })
 
-app.listen(process.env.PORT || 3000, () => console.log('App now running on port', server.adress().port));
+app.listen(process.env.PORT || 3000, () => console.log('App now running on port', app.adress().port));
